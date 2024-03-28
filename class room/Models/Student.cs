d88c26617d0d2c00591b769d12ffd34fde2fa1ca
muradramazanov02 <esrefsefiyev@gmail.com>
2024-03-28 12:05:55 +0400

    tapsiriq

---
+++ class room/Models/Student.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace class_room.Models
+{
+    internal class Student
+    {
+        public string Name;
+        public string Surname;
+        public string Group;
+        public int Point;
+        public Boolean isGraducated;
+
+
+        public Student(string name, string surname,string group, int point)
+        {
+            Name = name;
+            Surname = surname;  
+            Group = group;
+            Point = point;
+
+
+
+        }
+        public  Student(Boolean isgraducated)
+        {
+
+            isGraducated=isgraducated;
+
+
+        }
+        public void Getinfo()
+        {
+
+            Console.WriteLine($"{Group}-{Name} {Surname} {Point} ");
+
+        }
+        public  void CheckGraduation()
+        {
+            if (Point < 65)
+            {
+                Console.WriteLine("kesilib");
+            }
+            else
+            {
+                Console.WriteLine("mezundu");
+            }
+
+        }
+        public  void GetDiplomDegree()
+        {
+            if (Point < 65)
+            {
+                Console.WriteLine("yoxdur");
+            }
+            else if (Point > 65 && Point < 80)
+            {
+                Console.WriteLine("adi");
+            }
+            else if (Point > 80 && Point < 90)
+            {
+                Console.WriteLine("seref");
+            }
+            else 
+            {
+                Console.WriteLine("yusey seref");
+            }
+        }
+
+    }
+}
---
+++ class room/Program.cs
@@ -0,0 +1,21 @@
+﻿using class_room.Models;
+
+namespace class_room
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Student Nurlan= new Student("Nurlan","Abbasov","AB106",100);
+
+
+
+            Nurlan.Getinfo();
+            Nurlan.GetDiplomDegree();
+            Nurlan.CheckGraduation();
+
+
+            
+        }
+    }
+}
